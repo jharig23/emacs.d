f66b78068fc486025607f657067068d9ad8cbd9c
James Harig <jharig23@gmail.com>
2015-08-25 11:01:03 -0400

    Added pretty printing for xml

--- init.el
+++ init.el
@@ -16,6 +16,9 @@
 
 (transient-mark-mode nil)
 
+;; Open File at point keybinding
+(global-set-key (kbd "C-x g") 'find-file-at-point)
+
 ;; Org mode items that I like
 (global-set-key (kbd "C-C l") 'org-store-link)
 
@@ -33,6 +36,7 @@
  'org-babel-load-languages
  '((emacs-lisp . t)
    (ruby . t)
+   (java . t)
    (ditaa . t)
    (dot . t)
    (sh . t)
@@ -63,3 +67,31 @@
  )
 
 
+(defun my-org-screenshot ()
+  "Take a screenshot into a time stamped unique-named file in the
+same directory as the org-buffer and insert a link to this file."
+  (interactive)
+  (setq filename
+        (concat
+         (make-temp-name
+          (concat (buffer-file-name)
+                  "_"
+                  (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
+  (call-process "import" nil nil nil filename)
+  (insert (concat "[[" filename "]]")))
+
+
+(defun pretty-print-xml-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
+  (interactive "r")
+  (save-excursion
+      (nxml-mode)
+      (goto-char begin)
+      (while (search-forward-regexp "\>[ \\t]*\<" nil t) 
+        (backward-char) (insert "\n"))
+      (indent-region begin end))
+    (message "Ah, much better!"))
